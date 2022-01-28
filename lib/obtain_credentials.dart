dddb5ad2d3852d719f0331f1fb1d2320adfb0ee1
Nick Meinhold <nick@enspyr.co>
2022-01-28 13:12:54 +1100

    Reset Client ID and remove (cancelled) secret
    - felt shame at sharing a secret
    - updated obtainCredentials function to get the secret from a
       (gitignored) json file
    - updated json file with new secret

--- lib/obtain_credentials.dart
+++ lib/obtain_credentials.dart
@@ -1,25 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
 
 Future<AccessCredentials> obtainCredentials() async {
   final client = http.Client();
 
+  // TODO: make a model class with a fromJson
+  var credentialsString = File('credentials.json').readAsStringSync();
+  var credentials = jsonDecode(credentialsString);
+  var adventuverse = credentials['adventuverse'];
+  var desktopClient = adventuverse['desktopClient'];
+  var clientId = desktopClient['clientId'];
+  var clientSecret = desktopClient['clientSecret'];
+
   try {
     return await obtainAccessCredentialsViaUserConsent(
-      ClientId(
-          '384716656676-lkhc8q5n87b2jg023ma4mfi5496tgns5.apps.googleusercontent.com',
-          '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'),
+      ClientId(clientId, clientSecret),
       ['https://www.googleapis.com/auth/gmail.modify'],
       client,
-      _prompt,
+      (String url) {
+        print('Please go to the following URL and grant access:');
+        print('  => $url' r'&access_type=offline');
+        print('');
+      },
     );
   } finally {
     client.close();
   }
-}
-
-void _prompt(String url) {
-  print('Please go to the following URL and grant access:');
-  print('  => $url' r'&access_type=offline');
-  print('');
 }
